bfb846fd28f31294471fbfc147ab6831fadc00ba
Tran Nguyen Phu Nghia <nghiatran912005@gamil.com>
2025-01-18 14:18:43 +0700

    comment whitenoise module

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'members',
+    # 'members',
     'home',
 ]
 
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_tennis_club.urls'
